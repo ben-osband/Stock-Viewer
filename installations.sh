a154d4d69b6986ac85b1bd6e84df290773e387b4
Benjamin Osband <94814841+ben-osband@users.noreply.github.com>
2022-12-10 21:52:47 -0500

    Changed python3 to python in shell script

--- installations.sh
+++ installations.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
-python3 -m pip install matplotlib
-python3 -m pip install PySimpleGUI
-python3 -m pip install yahoo-finance
-python3 -m pip install numpy
+python -m pip install matplotlib
+python -m pip install PySimpleGUI
+python -m pip install yahoo-finance
+python -m pip install numpy
